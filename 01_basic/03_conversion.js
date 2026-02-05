484bf50853e91432ce8fa22a793d72eea5c1456f
tjangid8429 <tjangid8429@gmail.com>
2026-02-05 12:27:21 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueOfNumber = Number(score)
+// console.log(typeof valueOfNumber)
+// console.log(valueOfNumber)
+
+// let score = true
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueOfNumber = Number(score)
+// console.log(typeof valueOfNumber)
+// console.log(valueOfNumber)
+
+
+// let score = false
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueOfNumber = Number(score)
+// console.log(typeof valueOfNumber)
+// console.log(valueOfNumber)
+
+
+let score = "string"
+console.log(typeof score);
+console.log(typeof(score))
+
+let valueOfNumber = Number(score)
+console.log(typeof valueOfNumber)
+console.log(valueOfNumber)
