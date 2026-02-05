2e3378759fbe543ce961bd34dbf421ccc68a49a7
tjangid8429 <tjangid8429@gmail.com>
2026-02-05 13:09:57 +0000

    conversion of datatype

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -24,10 +24,22 @@
 // console.log(valueOfNumber)
 
 
-let score = "string"
-console.log(typeof score);
-console.log(typeof(score))
+// let score = "string"
+// console.log(typeof score);
+// console.log(typeof(score))
 
-let valueOfNumber = Number(score)
-console.log(typeof valueOfNumber)
-console.log(valueOfNumber)
+// let valueOfNumber = Number(score)
+// console.log(typeof valueOfNumber)
+// console.log(valueOfNumber)
+
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof stringNumber);
+hh
