bed46e569355d537b81430681733157b6851737e
tjangid8429 <tjangid8429@gmail.com>
2026-02-05 10:38:03 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all js code as newer js
+// alert (3+3) // this will give error because we are using node.js, not an browser
+// code readabilitu should be high 
+// console.log("Trun")
+
+// let name = "hitesh" -> string
+// let age = 18 -> number
+// let isLoggedIn = false -> boolen
+// let state; -> undefined
+
+// number = 2 to power 53
+// binint = very big number 
+// string = ""
+// boolean = True/false
+// null = standalone value
+// undefined =
+// symbol 
+
+
+// object
+
+// console.log(typeof  );  // show datatype
+
+console.log(typeof null);
+
