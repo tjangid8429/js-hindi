471e84c887ec35c1b621c1c4e3c5ddcc22bce2d0
tjangid8429 <tjangid8429@gmail.com>
2026-02-06 13:47:19 +0000

    datatype summary

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,39 @@
+// primitive
+
+// 7 type: string, number, boolean, null, undefined, symbol, bigint
+
+// const score = 100
+// const scoreValue = 300.3
+// const isLoggedIn = false
+// const outSideTemp = null
+// let usreEmail;
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// const bigInt = 1235428555545568542555555588524562565252252156525n
+
+// console.log(typeof bigInt);
+
+
+// reference (Non primitive)
+
+// type: Array, Object, Function
+
+const Heros = ["Tarun, Rahul, Virendra"]
+const objct = {
+    name:"Tarun",
+    age:21
+}
+
+const myfunc = function(){
+    console.log("Hellow World");
+}
+
+// console.log(typeof myfunc);
+
+
+// *************************Memories***************************
+
+// there are two type of memories 
+// 1. Stake  and   2. Heap
+
+
