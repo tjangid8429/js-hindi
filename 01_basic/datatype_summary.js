15c2805692a8a7a2d002d9b5af29ab6e7202ad8d
tjangid8429 <tjangid8429@gmail.com>
2026-02-06 14:09:17 +0000

    detatype summary

--- 01_basic/datatype_summary.js
+++ 01_basic/datatype_summary.js
@@ -34,6 +34,38 @@ const myfunc = function(){
 // *************************Memories***************************
 
 // there are two type of memories 
-// 1. Stake  and   2. Heap
+
+// 1. Stake  
+// - used in primitive datatype 
+// - when deffined in stake we get copy of variable 
+
+// 2. Heap
+// - used in not primitive datatype 
+// - when deffined in heap we get reffrance value of orignal value
 
 
+// example of stake
+let intaId = "rdx_tarun_0010"
+let anotherId = intaId
+anotherId = "rdxtarun"
+// console.log(anotherId);
+// console.log(intaId);
+
+
+// example of hape
+let userOne = {
+    gmail: "tarun@gmail.com",
+    upi: "tarun@upi"
+}
+
+let userTwo = userOne
+
+userTwo.gmail = "tarun123@gmail.com"
+
+// userTwo = {
+//     gmail: "tarun123@gmail.com",
+//     upi: "tarun123@upi"
+// }
+
+console.log(userOne.gmail)
+console.log(userTwo.gmail)
