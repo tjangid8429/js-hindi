6866a28402c0d159310b81cb74ce1d6304dc37fb
tjangid8429 <tjangid8429@gmail.com>
2026-02-06 10:07:10 +0000

    concersion

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -40,6 +40,43 @@
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(someNumber)
-console.log(typeof stringNumber);
-hh
+// console.log(someNumber)
+// console.log(typeof stringNumber);
+
+
+// *****************************Operations**************************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "Hellow"
+let str2 = " Tarun"
+let str3 = (str1 + str2)
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+1);
+// console.log(1+2+"5");
+// console.log(1+2+5+"7"+8+4);
+// console.log("1+2");
+// console.log(3+5*5%5);
+// console.log(3+5*(8%5));
+// console.log(true);
+// console.log(+true);
+// console.log("");
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter);
+
