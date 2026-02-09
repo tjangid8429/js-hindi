0b84f13a4f6e76326571c8b182f096960575111a
tjangid8429 <tjangid8429@gmail.com>
2026-02-09 13:57:35 +0000

    String

---
+++ 01_basic/05_string.js
@@ -0,0 +1,33 @@
+const name = "Tarun Jangid"
+const repoCount = 50 
+// console.log(name + repoCount + "value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Hiteshhc')
+
+// console.log (gameName[0]);
+// console.log (gameName.__proto__);
+// console.log (gameName.length);
+// console.log (gameName.toUpperCase());
+// console.log (gameName.charAt(2));
+// console.log (gameName.indexOf('t'));
+
+const newString = gameName.substring(0,4)
+// console.log (newString)
+
+const anotherString = gameName.slice(0,4)
+// console.log(anotherString)
+
+const newstringOne = "    hitesh   "
+// console.log(newstringOne);
+// console.log(newstringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20','_'));
+// console.log(url.includes('sunder'));
+
+
+
+
+
