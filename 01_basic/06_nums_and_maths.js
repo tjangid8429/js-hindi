645c682e44446cfc12f0494a49fe4a2566097280
tjangid8429 <tjangid8429@gmail.com>
2026-02-10 12:59:20 +0000

    num and math

---
+++ 01_basic/06_nums_and_maths.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score);
+
+const NewScore = new Number (600)
+// console.log(NewScore);
+
+// console.log(score.toString());
+//console.log(NewScore.toString().length);
+// console.log(score.toFixed(3));
+
+const otherNumber = 656.846
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// ********************************Maths********************************
+
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+console.log((Math.random()*10)+1);
+
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10 
+const max = 20 
+console.log((Math.random()*(max-min+1)));
+
+console.log(0.3293817869787869*(20-10+1));
