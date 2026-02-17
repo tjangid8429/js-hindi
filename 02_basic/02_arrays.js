655d3d324fb25f3583cb5499c6914ae3d10913da
tjangid8429 <tjangid8429@gmail.com>
2026-02-17 13:10:15 +0000

    Array

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,28 @@
+const marval_heros = [ "thor","Ironman","spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+// marval_heros.push(dc_heros)
+
+// console.log(marval_heros)
+// console.log(marval_heros[3][1]);
+
+// const allheros = marval_heros.concat(dc_heros)
+// console.log(allheros);
+
+// const allnewheros = [...marval_heros, ...dc_heros]
+// console.log(allnewheros);
+
+// const anarr = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const aslanarr = anarr.flat(Infinity)
+// console.log(aslanarr);
+
+// console.log(Array.isArray("Hitu"))
+// console.log(Array.from("Hitu"));
+// console.log(Array.from({name:"Hitu"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array Of(score1,score2,score3));
+
+
