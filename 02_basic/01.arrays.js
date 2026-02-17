eeb1c26273965e8ac756e7352e58279ecc781c20
tjangid8429 <tjangid8429@gmail.com>
2026-02-17 10:27:03 +0000

    Array

---
+++ 02_basic/01.arrays.js
@@ -0,0 +1,32 @@
+// Array
+
+const arr = [0,1,2,3,4,5,6]
+// const heros = ["rahul","virendra"]
+// const arr2 = new Array (1,2,3,4,)
+// console.log(arr2[1]);
+
+// Array method
+
+// arr.push(9,8)
+// arr.pop()
+// arr.unshift(8,6)
+// arr.shift()
+
+// console.log(arr.includes(5));
+// console.log(arr.includes(15));
+// console.log(arr.indexOf(5));
+// console.log(arr.indexOf(15));
+
+// const arr2 = arr.join()
+// console.log(arr2);
+// console.log (typeof arr2);
+
+// slice , splice
+
+// console.log("A", arr)
+// const myn1 = arr.slice(1,3)
+// console.log(myn1);
+
+// console.log("B", arr)
+// const myn2 = arr.splice(1,3)
+// console.log(myn2);
