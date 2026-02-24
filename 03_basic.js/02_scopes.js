a00e9f274155523a4bb8de8a10cd8af8a905ed84
tjangid8429 <tjangid8429@gmail.com>
2026-02-24 14:28:01 +0000

    scope

--- 03_basic.js/02_scopes.js
+++ 03_basic.js/02_scopes.js
@@ -4,8 +4,44 @@ let a = 30
 if(true){
     let a = 10
     const b = 20 
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
     
 }
 
-console.log(a);
+// console.log(a);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = " youtube"
+        // console.log(username + website);
+        // console.log(website)
+    }
+    
+    two()
+}
+one()
+
+
+if (true){
+    const username = "hitesh"
+    if(username === "hitesh"){
+        const website =" youtube"
+        // console.log(username+website)
+        // console.log(username);
+    }
+}
+
+
+// console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+const addTwo = function(num){
+    return num+2
+}
+// console.log(addTwo(5))
---
+++ 03_basic.js/03_arrow.js
@@ -0,0 +1,22 @@
+const user = {
+    // username: "hitesh",
+    price: 999,
+    welcomeMessage: function(){
+        // console.log(`${this.username}, welcome to website`)
+    }
+}
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// function chai (){
+//     let username= "Tarun"
+//     console.log(this.username)
+// }
+// chai()
+
+// const chai = () => {
+//     let username = "hitesh"
+//     console.log(this)
+// }
+// chai()
