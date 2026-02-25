5b0cbbff30033e1076dd69a02015ffebbb8ede27
tjangid8429 <tjangid8429@gmail.com>
2026-02-25 14:10:43 +0000

    iife

--- 03_basic.js/03_arrow.js
+++ 03_basic.js/03_arrow.js
@@ -20,3 +20,16 @@ const user = {
 //     console.log(this)
 // }
 // chai()
+
+// const addTwo = (num1,num2)=>{
+//     return num1 + num2
+// }
+// console.log(addTwo(3,4))
+
+// const addTwo = (num1,num2)=> num1 + num2
+
+// console.log(addTwo(3,4))
+
+const addTwo = (num1,num2)=>({username:"hitesh"})
+
+console.log(addTwo(3,4))
---
+++ 03_basic.js/04_iife.js
@@ -0,0 +1,12 @@
+(function chai(){
+    console.log('DB CONNECTED')
+})();
+
+(function ourcode(){
+    console.log('DB CONNECTED')
+})();
+
+((name)=>{
+    console.log('hgfds');
+    
+})()
