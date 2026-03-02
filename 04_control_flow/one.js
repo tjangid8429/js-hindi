ab4f79ab01ba7f0b8b11529eda3b6d72ccd4b807
tjangid8429 <tjangid8429@gmail.com>
2026-03-02 12:29:41 +0000

    controle flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+const isuserloggedin = true
+if (2!=3){
+    // console.log("executed");
+}
+
+const temperature = 41
+// if (temperature<50){
+//     console.log("less then 50")
+// }
+// console.log(`${temperature }is grater then 50` )
+
+
+if(temperature===41){
+    // console.log("less then 50")
+}else{
+    // console.log("temperature is greater than 50")
+}
+// console.log("Execute");
+
+
+const score = 200
+if (score>100){
+    const power = "fly"
+    // console.log(`User power :${power}`)
+}
+// console.log(`user power:${power}`);
+
+
+// const balance = 1000
+// if(balance>500)console.log("test"), console.log("test2");
+
+
+// const balance = 740
+// if (balance<500){
+//     console.log("less then 500");
+// }else if (balance < 750){
+//     console.log("less than 7500");
+// }else if (balance < 900){
+//     console.log("less than 750");
+// }else {
+//     console.log("it is 1000");
+// }
+
+
+// const userLoggedIn = true
+// const debitcard = true
+// const loggedInFromaGoogle = false
+// const loggedInFromEmail = true
+
+// if (userLoggedIn && debitcard && 2==3){
+//     console.log("allow to buy course")
+// }
+// if(loggedInFromaGoogle && loggedInFromEmail){
+//     console.log("user logged In");
+// }
+
+
+
+
